22a1a2bc699704ce0b05179edfddc6873a70515e
UsusCimex <d.lanin@g.nsu.ru>
2023-10-31 16:10:31 +0700

    add new query

--- computerComponents/query.sql
+++ computerComponents/query.sql
@@ -1,6 +1,7 @@
 --- 1. Вывод серийного номера компьютера и его стоимость.
 
-SELECT DISTINCT computer_serial AS serial_number, price_sell_computer AS price
+SELECT DISTINCT computer_serial AS serial_number, 
+       price_sell_computer AS price
 FROM computer_components
 ORDER BY serial_number;
 
@@ -8,16 +9,23 @@ ORDER BY serial_number;
 
 SELECT name, price, guarantee_period
 FROM components
-WHERE category_id = ( SELECT id FROM components WHERE name = 'AMD Ryzen 5 3600' )
-      AND name != 'AMD Ryzen 5 3600'
+WHERE category_id = ( 
+        SELECT id 
+        FROM components 
+        WHERE name = 'AMD Ryzen 5 3600'
+    ) AND name != 'AMD Ryzen 5 3600'
 ORDER BY price;
 
 --- 3. Найти самое дешевое комплектующее для каждой категории.
 
-SELECT components.name AS component, category.name AS category, price
-FROM components LEFT JOIN category ON category_id = category.id
+SELECT components.name AS component, 
+       category.name AS category, 
+       price
+FROM components 
+     LEFT JOIN category ON category_id = category.id
 WHERE (category_id, price) IN (
-    SELECT category_id, MIN(price)
+    SELECT category_id, 
+           MIN(price)
     FROM components
     GROUP BY category_id
 );
@@ -25,8 +33,10 @@ WHERE (category_id, price) IN (
 --- 4. Вывести комплектующие, которые находятся на первых 3 местах по уровню востребованности (наиболее часто используемые во всех собранных компьютерах). 
 --- Примечание: если уровень востребованности у двух комплектующих одинаковый, то обе находят-ся на одном месте.
 
-SELECT components.name, category.name
-FROM components LEFT JOIN category ON components.category_id = category.id
+SELECT components.name AS company_name, 
+       category.name AS category_name
+FROM components 
+     LEFT JOIN category ON components.category_id = category.id
 WHERE components.id IN (
     SELECT component_id
     FROM computer_components
@@ -40,12 +50,27 @@ WHERE components.id IN (
     )
 );
 
+SELECT components.name AS component_name, 
+       category.name AS category_name, 
+       sell_count
+FROM (
+    SELECT component_id,
+           COUNT(id) AS sell_count,
+           RANK() OVER (ORDER BY COUNT(id) DESC) AS rk
+    FROM computer_components
+    GROUP BY component_id
+) subQuery
+    LEFT JOIN components ON components.id = subQuery.component_id
+    LEFT JOIN category ON components.category_id = category.id
+WHERE rk <= 3;
+
 --- 5. Вывести компьютеры с рентабельностью свыше 30% (цена продажи на 30% больше стоимости производства).
 
 SELECT computer_serial, 
        SUM(components.price) AS components_price, 
        ROUND(AVG(price_sell_computer), 2) AS sell_price,
        ROUND(AVG(price_sell_computer) / SUM(components.price), 2) AS profitability
-FROM computer_components LEFT JOIN components ON component_id = components.id
+FROM computer_components
+     LEFT JOIN components ON component_id = components.id
 GROUP BY computer_serial
 HAVING AVG(price_sell_computer) / SUM(components.price) > 1.3;
